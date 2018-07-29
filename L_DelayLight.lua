39839a1e8e24dbf1b2511d91d9e913041a4686a0
Patrick Rigney <patrick@toggledbits.com>
2018-07-29 11:45:02 -0400

    Update for release

--- L_DelayLight.lua
+++ L_DelayLight.lua
@@ -10,7 +10,7 @@ module("L_DelayLight", package.seeall)
 local debugMode = false
 
 local _PLUGIN_NAME = "DelayLight"
-local _PLUGIN_VERSION = "1.5RC1"
+local _PLUGIN_VERSION = "1.5"
 local _PLUGIN_URL = "https://www.toggledbits.com/delaylight"
 local _CONFIGVERSION = 00109
 
