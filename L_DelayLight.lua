8003fab280bc280206e71b45cc6a3f4676877784
Patrick Rigney <patrick@toggledbits.com>
2019-04-07 15:50:36 -0400

    Update version for release 1.9

--- L_DelayLight.lua
+++ L_DelayLight.lua
@@ -11,7 +11,7 @@ local debugMode = false
 
 local _PLUGIN_ID = 9036
 local _PLUGIN_NAME = "DelayLight"
-local _PLUGIN_VERSION = "1.9develop-19091"
+local _PLUGIN_VERSION = "1.9"
 local _PLUGIN_URL = "https://www.toggledbits.com/delaylight"
 local _CONFIGVERSION = 00110
 
